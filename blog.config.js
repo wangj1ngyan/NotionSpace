5225c5e23cc325216be3bc23b2e9dbf04142ea55
wangj1ngyan <78077524+wangj1ngyan@users.noreply.github.com>
2024-03-03 22:38:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,4 +1,20 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/article/how-to-config-notion-next#c4768010ae7d44609b744e79e2f9959a
+
+// 定义支持的语言及其对应的欢迎语
+const greetingWords = {
+  'zh-CN': '你好，我是一个图形学在读学生',
+  'en-US': 'Hi，I\'m an upcoming programmer',
+  'fr-FR': 'Bonjour，j\'écris parfois en français',
+  'ja-JP': '日本語を勉強しているんだ',
+  'default': 'Welcome to my blog🎉' // 默认欢迎语
+};
+
+// 默认语言
+const defaultLanguage = process.env.NEXT_PUBLIC_LANG || 'zh-CN';
+
+// 根据当前语言选择对应的欢迎语
+const selectedGreeting = greetingWords[defaultLanguage] || greetingWords['default'];
+
 const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_PAGE_ID:
@@ -7,13 +23,14 @@ const BLOG = {
   NEXT_REVALIDATE_SECOND: process.env.NEXT_PUBLIC_REVALIDATE_SECOND || 5, // 更新内容缓存间隔 单位(秒)；即每个页面有5秒的纯静态期、此期间无论多少次访问都不会抓取notion数据；调大该值有助于节省Vercel资源、同时提升访问速率，但也会使文章更新有延迟。
   THEME: process.env.NEXT_PUBLIC_THEME || 'hexo', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   THEME_SWITCH: process.env.NEXT_PUBLIC_THEME_SWITCH || false, // 是否显示切换主题按钮
-  LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', 'en-US', 'fr-FR', 'ja-JP', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
+  LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN',// e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: 2023, // e.g if leave this empty, current year will be used.
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '你好，我是一个图形学在读学生,Hi，I\'m an upcoming programmer,Bonjour，j\'écris parfois en français,日本語を勉強しているんだ,Welcome to my blog🎉',
+  // GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '你好，我是一个图形学在读学生,Hi，I\'m an upcoming programmer,Bonjour，j\'écris parfois en français,日本語を勉強しているんだ,Welcome to my blog🎉',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || selectedGreeting,
   
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
